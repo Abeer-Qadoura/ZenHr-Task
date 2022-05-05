fd1c016e4266c68f8cdc39c07fe2ffba369b3a26
Abeer Qadoura <a.qadoura@certacure.com>
2022-05-05 03:08:59 +0300

    first commit

--- src/test/java/zenhr.java
+++ src/test/java/zenhr.java
@@ -7,13 +7,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.mongodb.assertions.Assertions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 public class zenhr {
 
@@ -44,18 +45,24 @@ public class zenhr {
 		List<WebElement> links = leftSideMenu.findElements(By.tagName("a"));
 		System.out.println("No of links are " + links.size());
 		// checking the links fetched.
-		
+
 		for (int i = 0; i < links.size(); i++) {
 			WebElement E1 = links.get(i);
 			String url = E1.getAttribute("href");
 			verifyLinks(url);
 		}
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector(".dropdown-menu__collapse")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[@class='user-menu__item__link '])[9]")));
+		driver.findElement(By.xpath("(//a[@class='user-menu__item__link '])[9]")).click();
 
 		driver.quit();
 	}
 
 	public static void verifyLinks(String linkUrl) {
-		//List<Integer> BrokenList = new ArrayList<>();
+		// List<Integer> BrokenList = new ArrayList<>();
 
 		try {
 			URL url = new URL(linkUrl);
@@ -69,8 +76,7 @@ public class zenhr {
 				int st3 = httpURLConnect.getResponseCode();
 				System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage() + "is a broken link");
 				BrokenList.add(st3);
-			
-				
+
 			}
 
 			// Fetching and Printing the response code obtained
